936a18b7fb89e3cbab316f854ac47c2d095a4137
Marta Solenne <martasolenne.sol@gmail.com>
2023-02-23 15:51:22 +0100

    views

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,2 +1,23 @@
 class ListsController < ApplicationController
+  def index
+    @list = List.all
+  end
+
+  def show
+    @list = List.find(params[:id])
+  end
+
+  def new
+    @list = List.new
+  end
+  
+  def create
+    @list = List.new(valid_params)
+  end
+
+  private
+
+  def valid_params
+    params.require(:list).permit(:name)
+  end
 end
---
+++ db/migrate/20230223135340_new_poster_name.rb
@@ -0,0 +1,5 @@
+class NewPosterName < ActiveRecord::Migration[7.0]
+  def change
+    rename_column :movies, :post_url, :poster_url
+  end
+end
